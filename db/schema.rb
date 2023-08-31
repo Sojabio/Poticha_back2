9cc7fcd387736663e945bc1de35c6096aee1b170
soja <solene.jabaud@gmail.com>
2023-08-31 15:31:18 +0200

    fix create

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_31_065519) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_31_095208) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_31_065519) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["jti"], name: "index_jwt_denylist_on_jti"
+  end
+
+  create_table "properties", force: :cascade do |t|
+    t.string "title"
+    t.integer "price"
+    t.text "description"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_properties_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Property.destroy_all
+User.destroy_all
+
+10.times do
+  User.create!(
+    email: Faker::Internet.unique.email,
+    password: "123456")
+end
+
+10.times do
+  Property.create!(
+    title: Faker::Book.title,
+    price: rand(100000...1000000000),
+    description: Faker::Quotes::Shakespeare.king_richard_iii_quote,
+    user_id: rand(User.first.id..User.last.id)
+  )
+end
