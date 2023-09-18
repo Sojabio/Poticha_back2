51568d202459d0537fe4074c88dc0523a1787e3b
soja <solene.jabaud@gmail.com>
2023-09-18 18:10:58 +0200

    try fix refresh

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
 class ApplicationController < ActionController::API
   def index
     respond_to do |format|
-      format.html { render body: Rails.root.join('public/index.html').read }
+      format.html { render body: Rails.root.join('index.html').read }
     end
   end
 end
-
