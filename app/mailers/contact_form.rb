3deaaf4d3ac9b8b823cdb4b875c348aea88ee6c9
soja <solene.jabaud@gmail.com>
2023-09-19 13:15:10 +0200

    reorganize mailer

--- app/mailers/contact_form.rb
+++ app/mailers/contact_form.rb
@@ -3,16 +3,16 @@ class ContactForm < ApplicationMailer
   default from: 'sojabio@protonmail.com'
 
   # send email from contact form
-  # def send_message_email(message)
-  #   @message = message
-  #   mail( to: 'solene.jabaud@gmail.com',
-  #   subject: 'Nouveau message de LePoticha' )
-  # end
+    def send_message_email(message, recipient_email)
+      @message = message
+      mail( to: 'solene.jabaud@gmail.com',
+      subject: 'Nouveau message de LePoticha' )
+    end
 
     # send email to author
     def send_message_email_to_author(message, recipient_email)
       @message = message
-      mail(to: recipient_email, subject: 'Nouveau message sur LePoticha') 
+      mail(to: recipient_email, subject: 'Nouveau message sur LePoticha')
     end
 
 
