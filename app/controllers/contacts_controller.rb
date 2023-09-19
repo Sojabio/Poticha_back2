2a623839fc2980ae22903aff45bee9656659132f
soja <solene.jabaud@gmail.com>
2023-09-19 12:09:42 +0200

    test mail author

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,12 +4,12 @@ class ContactsController < ApplicationController
   def create
     @message = contact_params
 
-    # recipient_email = @message[:recipientEmail]
-    # puts "Recipient Email: #{recipient_email}"
-    # send_email_to_author = ContactForm.send_message_email_to_author(@message, recipient_email).deliver
-    send_email = ContactForm.send_message_email(@message).deliver
+    recipient_email = @message[:recipientEmail]
+    puts "Recipient Email: #{recipient_email}"
+    send_email_to_author = ContactForm.send_message_email_to_author(@message, recipient_email).deliver
+    # send_email = ContactForm.send_message_email(@message).deliver
 
-    if send_email
+    if send_email_to_author
       render json: {sent: true}
     else
       render json: {sent: false}
--- app/mailers/contact_form.rb
+++ app/mailers/contact_form.rb
@@ -3,17 +3,19 @@ class ContactForm < ApplicationMailer
   default from: 'sojabio@protonmail.com'
 
   # send email from contact form
-  def send_message_email(message)
-    @message = message
-    mail( to: 'solene.jabaud@gmail.com',
-    subject: 'Nouveau message de LePoticha' )
-  end
+  # def send_message_email(message)
+  #   @message = message
+  #   mail( to: 'solene.jabaud@gmail.com',
+  #   subject: 'Nouveau message de LePoticha' )
+  # end
 
     # send email to author
-    # def send_message_email_to_author(message, recipient_email)
-    #   @message = message
-    #   mail( to: recipient_email,
-    #   subject: 'Nouveau message sur LePoticha' )
-    # end
+    def send_message_email_to_author(message, recipient_email)
+      @message = message
+      mail(to: recipient_email, subject: 'Nouveau message sur LePoticha') do |format|
+        format.text { render plain: "To: #{recipient_email}" }
+      end
+    end
+
 
 end
